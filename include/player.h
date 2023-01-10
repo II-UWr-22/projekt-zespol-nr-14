3afd9666063099b7b0e85f71cadb18f52ef9e3ef
qbojj <105227351+qbojj@users.noreply.github.com>
2023-01-10 12:11:00 +0100

    establish player_t members and uiHandler interface

--- include/player.h
+++ include/player.h
@@ -2,10 +2,15 @@
 #define PLAYER_H_
 
 #include "card.h"
+#include "stdint.h"
 
 typedef struct player_t
 {
-    
+    const char *name;
+    uint32_t validCards;
+    card_t cards[5]; 
+    uint64_t balance;
+    uint64_t bid;
 } player_t;
 
 #endif
