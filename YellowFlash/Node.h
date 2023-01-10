bd84e8a9eefa327832b6960dcc1d08105ef5ce70
YellowFlash1040 <andrejkovtunec@gmail.com>
2023-01-11 00:41:24 +0100

    Revert "I have created my variant of project and added it and want you to see it and comment"
    This reverts commit 080e6c829c3e09b18db799391f04b22da82f6589.

--- YellowFlash/Node.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Card.h"
-
-typedef struct Node
-{
-    Card Card;
-    Node* Next;
-}Node;
--- YellowFlash/Suit.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-enum Suit
-{
-	Spade,
-	Heart,
-	Diamond,
-	Club
-};
