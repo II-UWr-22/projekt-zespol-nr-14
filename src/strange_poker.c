92e62d8f9b8e170277e46129e72f88b0917cb7fa
Hammiee <118756612+Hammiee@users.noreply.github.com>
2023-01-24 00:46:18 +0100

    Adding bids and card dropin
    * the rest of bids
    
    * Fixing bugs
    
    * Fixing errors
    
    Co-authored-by: Hammie97 <hammie97.97@gmail.com>

--- src/strange_poker.c
+++ src/strange_poker.c
@@ -78,6 +78,38 @@ void bidPlayer( IuiHandler_t *ui, GameContext_t *ctx, uint32_t i, uint64_t *min_
     if( newBid != 0 ) ctx->moneyOnTable += newBid - lastBid;
 }
 
+void bidFunction(IuiHandler_t *ui, GameContext_t *ctx, uint64_t *min_bid, uint32_t *playerCnt, uint32_t start, player_t *players){
+    uint32_t current_players = *playerCnt;
+    for( uint32_t i=start; i < *playerCnt; i++)
+    {
+        if(players[i].validCards != 0 && players[i].bid != players[i].balance)
+            bidPlayer( ui, ctx, i, min_bid, &current_players );
+    }
+    uint32_t curPlayer = 0;
+    while( current_players != 0 && 
+        ( players[curPlayer].validCards == 0 || players[curPlayer].bid < *min_bid ) )
+            {
+                if( players[curPlayer].validCards != 0 && players[curPlayer].bid != players[curPlayer].balance )
+                    bidPlayer( ui, ctx, curPlayer, min_bid, &current_players );
+                    
+                curPlayer = ( curPlayer + 1 ) % *playerCnt;
+            }
+
+}
+
+void drop(IuiHandler_t *ui, GameContext_t *ctx, uint32_t *playerCnt, player_t *players){
+    for(uint32_t i=0; i< *playerCnt; i++){
+        if(players[i].validCards!=0){
+            ctx->currentPlayer = *playerCnt+1;
+            printMessage( ui, ctx, "Ready player %s! (%u)", players[i].name, i );
+            ctx->currentPlayer= i;
+            uint32_t drop=ui->drop (ui -> data, ctx);
+            players[i].cards[drop]=players[i].cards[players[i].validCards-1];
+            players[i].validCards--;
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     const char *backendName = "ncurses";
@@ -164,27 +196,41 @@ int main(int argc, char *argv[])
                 ctx.moneyOnTable += 20;
             }
             
-            // need to switch the first player to the last position after the end of the round
-            uint32_t current_players = playerCnt;
-            for( uint32_t i=2; i < playerCnt; i++)
-            {
-                bidPlayer( ui, &ctx, i, &min_bid, &current_players );
-            }
-
-            uint32_t curPlayer = 0;
-            while( current_players != 0 && 
-                    ( players[curPlayer].validCards == 0 || players[curPlayer].bid < min_bid ) )
-            {
-                if( players[curPlayer].validCards != 0 && players[curPlayer].bid != players[curPlayer].balance )
-                    bidPlayer( ui, &ctx, curPlayer, &min_bid, &current_players );
-                
-                curPlayer = ( curPlayer + 1 ) % playerCnt;
-            }
+            bidFunction(ui, &ctx, &min_bid, &playerCnt, 2, players);
 
             //flop
             ctx.visibleTableCards = 3;
             ctx.currentPlayer = playerCnt + 1; // do not show players' cards
             printMessage( ui, &ctx, "Flop!" );
+
+            //2nd bids
+            bidFunction(ui, &ctx, &min_bid, &playerCnt, 0, players);
+
+            //drop the 4th card
+            drop(ui, &ctx, &playerCnt, players);
+
+            //turn
+            ctx.visibleTableCards = 4;
+            printMessage( ui, &ctx, "Turn!" );
+            
+            //3rd bids
+            bidFunction(ui, &ctx, &min_bid, &playerCnt, 0, players);
+
+            //drop the 3rd card
+            drop(ui, &ctx, &playerCnt, players);
+
+            //river
+            ctx.visibleTableCards = 5;
+            printMessage( ui, &ctx, "River!" );
+            
+            //4th bids
+            bidFunction(ui, &ctx, &min_bid, &playerCnt, 0, players);
+
+            //check the winner
+
+            //add table money to the winners balance
+
+            //switch the first player to the last position after the end of the round
 
             // ?????
 
